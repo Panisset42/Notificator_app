08a84f684f72b4a28595b9dd495dea739e1b072b
Lucas Panisset <lucas.lima319@etec.sp.gov.br>
2023-12-30 23:14:46 -0300

    the basic notification functionality added, lacks timer and proper messages only

--- app/src/main/java/com/panisset/notificator/MainActivity.kt
+++ app/src/main/java/com/panisset/notificator/MainActivity.kt
@@ -1,101 +1,175 @@
 package com.panisset.notificator
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageButton
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.graphics.drawable.IconCompat
-import java.io.IOException
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : ComponentActivity() {
-    private val PERMISSION_REQUEST_CODE = 1
-    private var selectedImageUri: Uri? = null
+  private val imageArray = arrayOf(R.drawable.eduzz, R.drawable.hotmart, R.drawable.monettize, R.drawable.braip, R.drawable.kiwify, R.drawable.nubank, R.drawable.perfectpay)
 
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        if (uri != null) {
-            Log.d("PhotoPicker", "Selected URI: $uri")
-            selectedImageUri = uri
-            val imageButton: ImageButton = findViewById(R.id.imageButton)
-            imageButton.setImageURI(uri)
-        } else {
-            Log.d("PhotoPicker", "No media selected")
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.layout)
+    val imageButton: ImageView = findViewById(R.id.imageButton)
+    createNotificationChannel()
+    val notifyButton: Button = findViewById(R.id.Notify)
+
+
+    setImageFromDrawableArray(imageButton)
+
+    val spinnerOptions =
+      arrayOf("Nubak", "PerfectPay", "Monettize", "Kiwify", "Hotmart", "Eduzz", "Braip")
+
+    // Set up the spinner
+    val spinner: Spinner = findViewById(R.id.spinner)
+    val spinnerArrayAdapter =
+      ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerOptions)
+    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    spinner.adapter = spinnerArrayAdapter
+
+    // Handling spinner item selections
+    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+      override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+        val selectedItem = parent.getItemAtPosition(position).toString()
+        updateImage(imageButton, selectedItem)
+
+        // Assuming getImageResId returns the appropriate drawable resource ID
+        val imageResId = getImageResId(selectedItem)
+        showNotification("Selected Item", "You selected: $selectedItem", imageResId)
+      }
+
+      override fun onNothingSelected(parent: AdapterView<*>) {
+        // Optional: Handle the case where no item was selected
+      }
+    }
+    notifyButton.setOnClickListener {
+      val selectedPosition = spinner.selectedItemPosition
+      val selectedItem = spinnerOptions[selectedPosition]
+      val imageResId = getImageResId(selectedItem)
+      showNotification("Selected Item", "You selected: $selectedItem", imageResId)
     }
 
+  }
+    private fun updateImage(imageView: ImageView, selectedItem: String) {
+        val imageResId = when (selectedItem) {
+          "Nubak" -> R.drawable.nubank // Replace with your actual resource IDs
+          "PerfectPay" -> R.drawable.perfectpay
+          "Monettize" -> R.drawable.monettize
+          "Kiwify" -> R.drawable.kiwify
+          "Hotmart" -> R.drawable.hotmart
+          "Eduzz" -> R.drawable.eduzz
+          "Braip" -> R.drawable.braip
+          else -> R.drawable.nubank // Default or fallback image
+        }
+        imageView.setImageResource(imageResId)
+      }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout)
-        val image: ImageButton = findViewById(R.id.imageButton)
-        image.setOnClickListener { imagePicker(it) }
-        val notifyButton: Button = findViewById(R.id.Notify)
-        notifyButton.setOnClickListener { notifyUser() }
+      fun onNothingSelected(parent: AdapterView<*>) {
+        // Optional: Handle the case where no item was selected
+      }
+
+  private fun setImageFromDrawableArray(imageButton: ImageView) {
+    try {
+      val randomIndex = (0 until imageArray.size).random()
+      imageButton.setImageResource(imageArray[randomIndex])
+    } catch (e: Exception) {
+      Log.e("ImageError", "Error setting image: ${e.message}")
     }
-    private fun imagePicker(view: View) {
-        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PERMISSION_GRANTED) {
-            // Use PickVisualMediaRequest with ImageOnly
-            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        } else {
-            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
-        }
+  }
+
+
+  private fun createNotificationChannel() {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val name = "notify" // Directly using a string literal
+      val descriptionText = "notify_description"
+      val importance = NotificationManager.IMPORTANCE_DEFAULT
+      val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+        description = descriptionText
+      }
+      // Register the channel with the system
+      val notificationManager: NotificationManager =
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+      notificationManager.createNotificationChannel(channel)
+    }
+  }
+  private fun showNotification(title: String, content: String, iconResId: Int) {
+    val intent = Intent(this, MainActivity::class.java).apply {
+      flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
 
-    private fun notifyUser() {
-        val name = findViewById<EditText>(R.id.NameOfInstitution).text.toString()
-        val description = findViewById<EditText>(R.id.Description).text.toString()
-        val repeatCountText = findViewById<EditText>(R.id.numberOfiteractions).text.toString()
-        val repeatCount = repeatCountText.toIntOrNull() ?: 0
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // Create a notification channel (required for API 26+)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("your_channel_id", "Your Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        val builder = NotificationCompat.Builder(this, "your_channel_id")
-            .setContentTitle(name)
-            .setContentText(description)
-
-        selectedImageUri?.let { uri ->
-            val bitmap = uriToBitmap(uri)
-            bitmap?.let {
-                builder.setSmallIcon(IconCompat.createWithBitmap(bitmap))
-            }
-        }
-
-        // Notify
-        for (i in 1..repeatCount) {
-            notificationManager.notify(i, builder.build()) // Use 'i' as notification ID for uniqueness
-        }
+    val pendingIntentFlag = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    } else {
+      PendingIntent.FLAG_UPDATE_CURRENT
     }
-    private fun uriToBitmap(uri: Uri): Bitmap? {
-        return try {
-            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
 
-            // Resize bitmap to 24x24 dp (which might be different in pixels depending on the screen density)
-            val sizeInDp = 24
-            val scale = resources.displayMetrics.density
-            val sizeInPx = (sizeInDp * scale + 0.5f).toInt()
-            Bitmap.createScaledBitmap(bitmap, sizeInPx, sizeInPx, true)
-        } catch (e: IOException) {
-            Log.e("MainActivity", "Error converting Uri to Bitmap", e)
-            null
-        }
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, pendingIntentFlag)
 
+    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+      .setSmallIcon(iconResId) // Use the selected image as small icon
+      .setContentTitle(title)
+      .setContentText(content)
+      .setContentIntent(pendingIntent)
+      .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+      .build()
+
+    val notificationId = System.currentTimeMillis().toInt()
+
+    // Post the notification
+    val notificationManager = NotificationManagerCompat.from(this)
+    if (ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.POST_NOTIFICATIONS
+      ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      // TODO: Consider calling
+      //    ActivityCompat#requestPermissions
+      // here to request the missing permissions, and then overriding
+      //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+      //                                          int[] grantResults)
+      // to handle the case where the user grants the permission. See the documentation
+      // for ActivityCompat#requestPermissions for more details.
+      return
     }
+    notificationManager.notify(notificationId, notification)
+
+  }
+
+  companion object {
+
+    const val CHANNEL_ID = "first"
+    const val NOTIFICATION_ID = 101
+  }
+
+
+private fun getImageResId(selectedItem: String): Int {
+  return when (selectedItem) {
+    "Nubak" -> R.drawable.nu
+    "PerfectPay" -> R.drawable.perfectpay
+
+    else -> R.drawable.nubank // Default or fallback image
+  }
+}
+
+
 }
